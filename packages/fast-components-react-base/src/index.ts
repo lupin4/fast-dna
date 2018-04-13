87a5b85780e8152c1e9eb744c108fee66b102da6
Nick Rice <nicholas.rice119@gmail.com>
2018-04-13 16:31:46 -0700

    adding checkbox

--- packages/fast-components-react-base/src/index.ts
+++ packages/fast-components-react-base/src/index.ts
@@ -2,9 +2,22 @@ import Button from "./button";
 export { Button };
 export * from "./button";
 
+
+import ContextMenu from "./context-menu";
+export { ContextMenu };
+export * from "./context-menu";
+
 import ContextMenuItem from "./context-menu-item";
 export { ContextMenuItem };
 export * from "./context-menu-item";
+
+import ContextMenuItemCheckbox from "./context-menu-item-checkbox";
+export { ContextMenuItemCheckbox };
+export * from "./context-menu-item-checkbox";
+
+import ContextMenuItemRadio from "./context-menu-item-radio";
+export { ContextMenuItemRadio};
+export * from "./context-menu-item-radio";
 
 import Divider from "./divider";
 export { Divider };
